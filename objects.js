c5ffe5e1b7420c00bb690ff9521f3d839a4c0956
Hemalatha M <hemalatha.psna@gmail.com>
2018-12-23 20:13:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,14 +1,14 @@
 var playlist={Sundari:'Ilayaraja', orukal:'Yuvan'};
 
-function updatePlaylist(playlist, artistName, songTitle) {
+/*function updatePlaylist(playlist, artistName, songTitle) {
   playlist.songTitle=artistName;
   return playlist;
    //return Object.assign({}, playlist, {[Mustafa]:"AR Rehman"});
-}
-
-/*function updatePlaylist(playlist, artistName, songTitle) {
-    return Object.assign({}, playlist, {[Mustafa]:"AR Rehman"});
 }*/
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    return Object.assign({}, playlist, {[songTitle]:artistName});
+}
 
 function removeFromPlaylist(playlist, artistName) {
   playlist=playlist.delete[artistName];
